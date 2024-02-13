3c7c06b9ebc75dbb3586b25408511526a2aeba50
Philippe Cuerrier <philippe.cuerrier@gmail.com>
2024-02-13 15:23:09 -0500

    Basic vector struct

---
+++ src/vector.h
@@ -0,0 +1,14 @@
+#pragma once
+
+struct vec2_t
+{
+    float x = 0;
+    float y = 0;
+};
+
+struct vec3_t
+{
+    float x = 0;
+    float y = 0;
+    float z = 0;
+};
---
+++ test/vector-test.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+#include "vector.h"
+
+TEST(Vector, dummy)
+{
+    EXPECT_TRUE(true);
+}
